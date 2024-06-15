681666dbb3d2a214f7f27e0e84dd643a0c2fc34c
Ganesh Musne <musaneganesh444@gmail.com>
2024-06-15 12:13:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/goals', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const GoalSchema = new mongoose.Schema({
+  date: { type: String, unique: true },
+});
+
+const Goal = mongoose.model('Goal', GoalSchema);
+
+app.post('/api/goals', async (req, res) => {
+  try {
+    const { date } = req.body;
+    const goal = new Goal({ date });
+    await goal.save();
+    res.status(201).send(goal);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get('/api/goals', async (req, res) => {
+  try {
+    const goals = await Goal.find({});
+    res.status(200).send(goals);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
